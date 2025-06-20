482284e0682448327292bdf1534c37e3c6f1a413
law2hill <149711558+law2hill@users.noreply.github.com>
2025-06-20 12:13:17 +0300

    again new xcode

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
